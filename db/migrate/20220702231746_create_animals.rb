f54ba5e702def6541665d7e76f6889387ab27221
Greg Khmara <gkhmara@gmail.com>
2022-07-02 16:37:12 -0700

    update migration

--- db/migrate/20220702231746_create_animals.rb
+++ db/migrate/20220702231746_create_animals.rb
@@ -1,6 +1,9 @@
 class CreateAnimals < ActiveRecord::Migration[6.0]
   def change
     create_table :animals do |t|
+      t.column :name, :string
+      t.column :age, :integer
+      t.column :breed, :string
     end
   end
 end
