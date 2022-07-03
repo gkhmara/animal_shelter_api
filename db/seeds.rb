7a270a242d94c6d82b5aeee1987130f3470629f7
Greg Khmara <gkhmara@gmail.com>
2022-07-02 17:12:48 -0700

    create code to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_animals
+  end
+
+  def generate_animals
+    20.times do |i|
+      animal = Animal.create!(
+        name: Faker::Creature::Dog.name,
+        age: Faker::Creature::Dog.age,
+        breed: Faker::Creature::Dog.breed
+      )
+      puts "Animal #{i}: Name is #{animal.name}, age is #{animal.age}, and breed is #{animal.breed}."
+    end
+  end
+end
+
+Seed.begin
