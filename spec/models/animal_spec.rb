90be43c119a3d03311092f54b190d485f2036830
Greg Khmara <gkhmara@gmail.com>
2022-07-03 13:46:42 -0700

    add test for validation

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -5,4 +5,7 @@ describe Animal do
     animal = Animal.create(:name => "Fido", :age => "young", :breed => "cute")
     expect(animal.name).to eq "Fido"
   end
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :age }
+  it { should validate_presence_of :breed }
 end
